2c525279ca5e7858ab22974a61853c6520a1b617
Peter Baumgartner <pete@lincolnloop.com>
2017-09-05 13:49:19 -0600

    Root logger tests

---
+++ logging_playground/test_root.py
@@ -0,0 +1,98 @@
+import logging
+from textwrap import dedent
+
+from logging_playground.utils import BaseLoggingTest
+
+
+class TestRoot(BaseLoggingTest):
+    """
+    What does the "root" logger do?
+    """
+
+    def test_root_keyword(self):
+        """
+        You can use the ``root`` keyword to configure the root logger
+        """
+        logging.config.dictConfig({
+            'version': 1,
+            'root': {
+                'level': 'ERROR',
+            },
+        })
+
+        self.assertLogTree(dedent("""
+            <--""
+               Level ERROR"""))
+
+    def test_root_as_empty_logger(self):
+        """
+        You can also use an empty logger name
+        """
+        logging.config.dictConfig({
+            'version': 1,
+            'loggers': {
+                '': {
+                    'level': 'ERROR',
+                }
+            },
+        })
+
+        self.assertLogTree(dedent("""
+            <--""
+               Level ERROR"""))
+
+    def test_root_applies_globally(self):
+        """
+        All modules use the root logger
+        """
+        logging.config.dictConfig({
+            'version': 1,
+            'handlers': {
+                'test': {
+                    'class': 'logging_playground.utils.MockLoggingHandler',
+                },
+            },
+            'loggers': {
+                '': {
+                    'handlers': ['test'],
+                },
+            }
+        })
+
+        log_msg = "This is a test"
+
+        # root applies to modules that aren't defined
+        logging.getLogger('not_defined_module').error(log_msg)
+        self.assertMockLog(logging.ERROR, [log_msg])
+
+    def test_root_inheritance(self):
+        """
+        Inheritance works the same with the root logger
+        """
+        logging.config.dictConfig({
+            'version': 1,
+            'handlers': {
+                'test': {
+                    'class': 'logging_playground.utils.MockLoggingHandler',
+                },
+                'test2': {
+                    'class': 'logging_playground.utils.MockLoggingHandler2',
+                },
+            },
+            'loggers': {
+                '': {
+                    'handlers': ['test'],
+                },
+                'my_module': {
+                    'handlers': ['test2']
+                }
+            }
+        })
+
+        log_msg = "This is a test"
+
+        logging.getLogger('my_module').error(log_msg)
+        # logs to its own logger
+        self.assertMockLog2(logging.ERROR, [log_msg])
+        # ...as well as the root logger
+        self.assertMockLog(logging.ERROR, [log_msg])
