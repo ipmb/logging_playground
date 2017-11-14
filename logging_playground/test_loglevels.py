04171a6eb806cd67a587cd74d32a708cc3953785
Peter Baumgartner <pete@lincolnloop.com>
2017-11-14 10:11:44 -0700

    Testing NOTSET

--- logging_playground/test_loglevels.py
+++ logging_playground/test_loglevels.py
@@ -98,3 +98,32 @@ class TestLogLevels(BaseLoggingTest):
         # Logs to both `my_module` and `my_module.child`
         logging.getLogger('my_module.child').warning(log_msg)
         self.assertMockLog(logging.WARNING, [log_msg, log_msg])
+
+    def test_notset_level(self):
+        """
+        Logging can be disabled altogether by using the `NOTSET` level
+        """
+        logging.config.dictConfig({
+            'version': 1,
+            'handlers': {
+                'test': {
+                    'class': 'logging_playground.utils.MockLoggingHandler',
+                    'level': 'DEBUG'
+                },
+            },
+            'loggers': {
+                'my_module': {
+                    'handlers': ['test'],
+                    'level': 'NOTSET',
+                },
+            }
+        })
+
+        log_msg = "This is a test"
+
+        logging.getLogger('my_module').info(log_msg)
+        # Does not log on debug level
+        self.assertMockLog(logging.INFO, [])
+        # Doesn't even log if we use NOTSET as the level
+        logging.getLogger('my_module').log(logging.NOTSET, log_msg)
+        self.assertMockLog(logging.NOTSET, [])
