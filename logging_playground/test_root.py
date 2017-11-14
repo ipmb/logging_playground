e61e8ff5619ffdd69f9e03e6fde259326f93abbd
Peter Baumgartner <pete@lincolnloop.com>
2017-11-14 10:17:26 -0700

    root propagation

--- logging_playground/test_root.py
+++ logging_playground/test_root.py
@@ -96,3 +96,69 @@ class TestRoot(BaseLoggingTest):
         self.assertMockLog2(logging.ERROR, [log_msg])
         # ...as well as the root logger
         self.assertMockLog(logging.ERROR, [log_msg])
+
+    def test_root_inheritance(self):
+        """
+        Inheritance works the same with the root logger
+        """
+        logging.config.dictConfig({
+            'version': 1,
+            'handlers': {
+                'test': {
+                    'class': 'logging_playground.utils.MockLoggingHandler',
+                },
+                'test2': {
+                    'class': 'logging_playground.utils.MockLoggingHandler2',
+                },
+            },
+            'loggers': {
+                '': {
+                    'handlers': ['test'],
+                },
+                'my_module': {
+                    'handlers': ['test2']
+                }
+            }
+        })
+
+        log_msg = "This is a test"
+
+        logging.getLogger('my_module').error(log_msg)
+        # logs to its own logger
+        self.assertMockLog2(logging.ERROR, [log_msg])
+        # ...as well as the root logger
+        self.assertMockLog(logging.ERROR, [log_msg])
+
+    def test_no_root_inheritance(self):
+        """
+        Don't propagate to root logger
+        """
+        logging.config.dictConfig({
+            'version': 1,
+            'handlers': {
+                'test': {
+                    'class': 'logging_playground.utils.MockLoggingHandler',
+                },
+                'null': {
+                    'class': 'logging.NullHandler',
+                },
+            },
+            'loggers': {
+                '': {
+                    'handlers': ['test'],
+                },
+                'my_module': {
+                    'propagate': False,
+                    'handlers': ['null'],
+                }
+            }
+        })
+
+        log_msg = "This is a test"
+
+        logging.getLogger('my_module').error(log_msg)
+        # does not log anywhere
+        self.assertMockLog(logging.ERROR, [])
+        # children don't either
+        logging.getLogger('my_module.child').error(log_msg)
+        self.assertMockLog(logging.ERROR, [])
